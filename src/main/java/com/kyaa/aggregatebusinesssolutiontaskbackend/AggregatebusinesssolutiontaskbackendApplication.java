ac402a7145a4c6b0d3f00238abc407ac87883c7e
KabirYusuf <sonkaybee@gmail.com>
2023-05-20 03:28:20 +0100

    task initialized

---
+++ src/main/java/com/kyaa/aggregatebusinesssolutiontaskbackend/AggregatebusinesssolutiontaskbackendApplication.java
@@ -0,0 +1,13 @@
+package com.kyaa.aggregatebusinesssolutiontaskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatebusinesssolutiontaskbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregatebusinesssolutiontaskbackendApplication.class, args);
+	}
+
+}
