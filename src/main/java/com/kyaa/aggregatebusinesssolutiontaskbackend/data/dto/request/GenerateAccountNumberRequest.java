07c28a11e1bdd962c454b4a486641c071da2b356
KabirYusuf <sonkaybee@gmail.com>
2023-05-20 04:15:50 +0100

    validation added to request dto

--- src/main/java/com/kyaa/aggregatebusinesssolutiontaskbackend/data/dto/request/GenerateAccountNumberRequest.java
+++ src/main/java/com/kyaa/aggregatebusinesssolutiontaskbackend/data/dto/request/GenerateAccountNumberRequest.java
@@ -1,11 +1,20 @@
 package com.kyaa.aggregatebusinesssolutiontaskbackend.data.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class GenerateAccountNumberRequest {
+    @NotBlank(message = "Bank code is Mandatory")
+    @Min(value = 3)
+    @Max(value = 3)
     private String bankCode;
+    @NotBlank(message = "Serial number is Mandatory")
+    @Min(value = 9)
+    @Max(value = 9)
     private String serialNumber;
 }
--- src/test/java/com/kyaa/aggregatebusinesssolutiontaskbackend/service/implementation/NigerianBankAccountNumberServiceTest.java
+++ src/test/java/com/kyaa/aggregatebusinesssolutiontaskbackend/service/implementation/NigerianBankAccountNumberServiceTest.java
@@ -1,7 +1,39 @@
 package com.kyaa.aggregatebusinesssolutiontaskbackend.service.implementation;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.kyaa.aggregatebusinesssolutiontaskbackend.data.dto.request.GenerateAccountNumberRequest;
+import com.kyaa.aggregatebusinesssolutiontaskbackend.data.model.BankAccountNumber;
+import com.kyaa.aggregatebusinesssolutiontaskbackend.data.repository.BankAccountNumberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 
 class NigerianBankAccountNumberServiceTest {
+    @InjectMocks
+    private NigerianBankAccountNumberService nigerianBankAccountNumberService;
+    @Mock
+    private BankAccountNumberRepository bankAccountNumberRepository;
+    @Captor
+    private ArgumentCaptor<BankAccountNumber> bankAccountNumberArgumentCaptor;
+    private GenerateAccountNumberRequest generateAccountNumberRequest;
+    @BeforeEach
+    void setup(){
+        generateAccountNumberRequest = new GenerateAccountNumberRequest();
+        generateAccountNumberRequest.setSerialNumber("999999999");
+        generateAccountNumberRequest.setBankCode("011");
+    }
 
+
+    @Test
+    void testThatAccountNumberCanBeGenerated(){
+        given(bankAccountNumberRepository)
+    }
 }
